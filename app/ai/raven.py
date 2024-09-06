1defa1c657d08920f94688ad2ff236549199d568
Joshua Jones <jjones@edoclogic.com>
2024-09-06 14:12:59 -0400

    fix: Implement execute_command function to handle different AI actions

--- app/ai/raven.py
+++ app/ai/raven.py
@@ -1,11 +1,10 @@
-import json
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import AIMessage
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from tools.file_operations import read_prompt_from_file
-from .memory import insert_document, search_similarity, delete_documents_by_query
-from .tools import process_tools_command
+from tools.file_operations import read_prompt_from_file, extract_function_call, clean_string
+from .memory import save_data, search_data, delete_data
+
 
 model = ChatOpenAI(model="gpt-4o")
 
@@ -20,13 +19,38 @@ def get_message_placeholder(messages: list[dict[str, any]]):
     return message_history
 
 
+def online_search(query: str):
+    # This function should be implemented to search the web for the query
+    return "I'm sorry, I'm not able to search the web at this time."
+
+
+def respond(query: str):
+    return clean_string(query)
+
+
+def execute_command(command, inquiry: str):
+    function_map = {
+        "searchdata": search_data,
+        "savedata": save_data,
+        "deletedata": delete_data,
+        "onlinesearch": online_search,
+        "respond": respond,
+    }
+
+    if command in function_map:
+        print(f"Executing function {command}({inquiry})")
+        return function_map[command](inquiry)
+    else:
+        print(f"Function {command} not recognized.")
+
+
 def general_chat_raven(messages: list[dict[str, any]]):
-    system_prompt = read_prompt_from_file('raven.md')
-    # memory_prompt = read_prompt_from_file('memory.md')
+    character_prompt = read_prompt_from_file('character.md')
+    functions_prompt = read_prompt_from_file('functions.md')
 
     formatted_messages = get_message_placeholder(messages)
 
-    full_prompt = system_prompt
+    full_prompt = character_prompt + "\n\n" + functions_prompt
 
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -39,9 +63,15 @@ def general_chat_raven(messages: list[dict[str, any]]):
 
     response = chain.invoke({"messages": formatted_messages})
     ai_response = response.content
+    print("Initial response: "+ai_response)
+    function_name, arguments = extract_function_call(ai_response)
 
-    # if the response starts with TOOLS then call the tools function
-    if ai_response.startswith('TOOLS'):
-        ai_response = process_tools_command(ai_response)
+    if function_name == None:
+        return ai_response
+
+    print("Function name: "+function_name)
+    print("Arguments: "+arguments)
+    if function_name:
+        ai_response = execute_command(function_name, arguments)
 
     return ai_response
